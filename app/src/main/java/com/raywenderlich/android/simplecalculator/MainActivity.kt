cd577a68f114168dd9d3e08247985edc1bc75657
German Muller <muller.german@gmail.com>
2022-11-12 00:02:42 -0300

    New Workflow6

--- app/src/main/java/com/raywenderlich/android/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/simplecalculator/MainActivity.kt
@@ -60,7 +60,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     registerButtonClick()
     textResult.isSelected = true
-    Log.e("TAG","HOlaaaa5!!")
+    Log.e("TAG","HOlaaaa6!!")
   }
 
   /**
