01a274b63164942b8b42516d3d27bb6fda5323ec
sebas <sebastian.muller@endava.com>
2022-11-15 14:06:28 -0300

    Test WorkFlow DevWeek1

--- app/src/main/java/com/raywenderlich/android/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/simplecalculator/MainActivity.kt
@@ -60,7 +60,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     registerButtonClick()
     textResult.isSelected = true
-    Log.e("TAG","HolaaaaSebiVm!!")
+    Log.e("TAG","HolaaaaDevWeek!!")
   }
 
   /**
