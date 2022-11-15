e673e4a07fae159258a2945d68ce076f0c4deaf2
sebas <sebastian.muller@endava.com>
2022-11-15 14:25:53 -0300

    workflow devweek2

--- app/src/main/java/com/raywenderlich/android/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/simplecalculator/MainActivity.kt
@@ -60,7 +60,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     registerButtonClick()
     textResult.isSelected = true
-    Log.e("TAG","HolaaaaDevWeek!!")
+    Log.e("TAG","HolaaaaDevWeek2!!")
   }
 
   /**
